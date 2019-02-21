223cab4979119f44b1b567b933ae980143d81112
Kristofer Karlsson <kristofer.karlsson@gmail.com>
2019-02-21 11:18:19 +0100

    WIP

--- rival_cpu.py
+++ rival_cpu.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+# Depends on python-psutil
+import psutil
+
 import time
 from itertools import cycle
 
@@ -10,19 +13,27 @@ delta = 0.05
 
 speed = 2
 
+idle_color = (0, 0, 0)
 config = [
-  (5, (0, 0, 255)),
-  (5, (0, 255, 255)),
-  (5, (0, 255, 0)),
-  (5, (255, 255, 0)),
-  (5, (255, 255, 255)),
-  (5, (255, 0, 255)),
+  (0.5, (20, 0, 0)),
+  (0.5, (255, 0, 0)),
 ]
 
 def interpolate(t, a, b):
   if t > 1: t = 1
   if t < 0: t = 0
   return tuple( int((1 - t) * x + t * y) for (x, y) in zip(a, b))
+
+def clamp(x, a, b):
+   if x < a: return a
+   if x > b: return b
+   return x
+
+def cpu_usage():
+  x = psutil.cpu_percent()
+  if x < 20: return 0
+  if x >= 50: return 100
+  return 50
 
 while True:
   profile = cli.get_plugged_mouse_profile()
@@ -33,12 +44,17 @@ while True:
     try:
       instance = mouse.RivalMouse(profile)
       prev = (0, 0, 0)
+      percentage = 0
       for (total_time, color) in cycle(config):
          total_time *= speed
          t = 0
          while t < total_time:
            c = interpolate(t / total_time, prev, color)
-           instance.set_color(*c)
+
+           percentage = percentage + clamp(cpu_usage() - percentage, -0.1, 0.1)
+           c2 = interpolate(percentage / 100.0, idle_color, c)
+
+           instance.set_color(*c2)
            t += delta
            time.sleep(delta)
          prev = color
